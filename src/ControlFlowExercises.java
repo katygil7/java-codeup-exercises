0b459725a84ff9de01df8af726fdd19b9dbb8dc3
katygil7 <gil.katherine22@gmail.com>
2022-08-02 15:33:06 -0500

    Do while complete

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,9 +1,15 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 5;
-        while(i <= 15){
-            System.out.print(i + " ");
-            i++;
-        }
+        int i = 2;
+
+        do{
+//            i *= i;
+//            i % 2 == 100;
+            System.out.println(i + " ");
+            i = (int) Math.pow(i,2);
+
+        }while (i <= 100000);
+
+
     }
 }
