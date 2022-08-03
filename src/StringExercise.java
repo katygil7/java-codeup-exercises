3680cf7cd314aaf774278b734d0a6ed86d47dbf6
katygil7 <gil.katherine22@gmail.com>
2022-08-03 14:30:10 -0500

    edited string basics 1

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -2,11 +2,10 @@ import java.util.Scanner;
 
 public class StringExercise {
     public static void main(String[] args) {
-       String value1 = "We don't need no education\nWe don't need no thought control";
-       String value2  = "Check \"this\" out!, \"s inside of \"s!";
-       String value3 = "In windows, the main drive is usually C:\\";
-       String value4 = "I can do backslashes \\, double backslashes \\\\,\n" +
+       String printMessage = "We don't need no education\nWe don't need no thought control\nCheck \"this\" out!, \"s inside of \"s!" +
+               "\nIn windows, the main drive is usually C:\\\nI can do backslashes \\, double backslashes \\\\,\n" +
                "and the amazing triple backslash \\\\\\!";
-       System.out.print(value1 + "\n" + value2 + "\n" + value3 + "\n" + value4);
+       System.out.print(printMessage);
     }
 }
+
