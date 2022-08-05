4b062461feeaa81d75f13132e1527fa7d9df4a70
katygil7 <gil.katherine22@gmail.com>
2022-08-05 10:00:43 -0500

    completed exercise 3

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class MethodsExercises {
 
     public static void main(String[] args) {
@@ -5,6 +7,11 @@ public class MethodsExercises {
         System.out.println(Subtraction(2,2));
         System.out.println(Multiplication(2,2));
         System.out.println(Division(2,2));
+        System.out.println(LoopMultiplication(2,2));
+        System.out.println(Recursion(8,2));
+        System.out.println(Modulus(2,4));
+        System.out.println(getInteger(1,10));
+
 
     }
 
@@ -21,4 +28,40 @@ public class MethodsExercises {
     public static int Division(int x, int y){
         return x / y;
     }
+    public static int LoopMultiplication ( int x, int y){
+        int z = 0;
+        for (int i = 0; i < x; i++){
+            z += y;
+        }
+        return z;
+    }
+    public static int Recursion(int x, int y){
+        if (y > 0){
+            return x + Recursion(x , y - 1);
+        }else{
+            return 0;
+        }
+    }
+    public static int Modulus (int x, int y){
+        return x % y;
+    }
+
+    public static int getInteger(int min, int max){
+        int track  = 0;
+        do {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter a number between 1 and 10: ");
+            int userInputNum = scanner.nextInt();
+
+            if (userInputNum >= min && userInputNum <= max) {
+                System.out.println("Number is within range");
+                System.out.println(userInputNum);
+                track = 1;
+            } else {
+                System.out.println("Number not in range");
+            }
+        } while (track == 0);
+        return track;
+    }
+
 }
