b0dba03f24ddba795908d450e6e8c1565781a12b
katygil7 <gil.katherine22@gmail.com>
2022-08-04 19:03:57 -0500

    finished Basic arithmetic

---
+++ src/MethodsExercises.java
@@ -0,0 +1,24 @@
+public class MethodsExercises {
+
+    public static void main(String[] args) {
+        System.out.println(Addition(2,2));
+        System.out.println(Subtraction(2,2));
+        System.out.println(Multiplication(2,2));
+        System.out.println(Division(2,2));
+
+    }
+
+    public static int Addition(int x, int y) {
+        return x + y;
+    }
+
+    public static int Subtraction(int x, int y) {
+        return x - y;
+    }
+    public static int Multiplication(int x, int y){
+        return x * x;
+    }
+    public static int Division(int x, int y){
+        return x / y;
+    }
+}
