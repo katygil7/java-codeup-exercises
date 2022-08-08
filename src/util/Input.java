86bd3c362c17005dfb73caff3dc5048ad22049e1
katygil7 <gil.katherine22@gmail.com>
2022-08-08 15:59:17 -0500

    finished Boolean yesNo

--- src/util/Input.java
+++ src/util/Input.java
@@ -3,6 +3,22 @@ package util;
 import java.util.Scanner;
 
 public class Input {
-    private Scanner scanner;
+    private Scanner scanner = new Scanner(System.in);
+public String getString(){
+    return this.scanner.nextLine();
+    }
+public boolean yesNo (){
+    String userInput = scanner.nextLine();
+    if ( userInput.equalsIgnoreCase("yes")){
+        return  true;
+    }else{
+        return false;
+    }
+}
 
+    public static void main(String[] args) {
+        System.out.println("Do you like pizza?");
+        Input UserInput = new Input();
+        System.out.println(UserInput.yesNo());
+    }
 }
