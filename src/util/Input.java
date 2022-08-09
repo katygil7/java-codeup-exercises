a480f4b669270a25d9d8fcb86f84c5a7fd382eea
katygil7 <gil.katherine22@gmail.com>
2022-08-09 09:32:23 -0500

    finished min max

--- src/util/Input.java
+++ src/util/Input.java
@@ -3,22 +3,40 @@ package util;
 import java.util.Scanner;
 
 public class Input {
-    private Scanner scanner = new Scanner(System.in);
+    private Scanner scanner;
+
+    public Input (){
+        this.scanner = new Scanner(System.in);
+    }
 public String getString(){
-    return this.scanner.nextLine();
+    return scanner.nextLine();
     }
 public boolean yesNo (){
-    String userInput = scanner.nextLine();
-    if ( userInput.equalsIgnoreCase("yes")){
+    String userInput = this.scanner.nextLine();
+    if ( userInput.equalsIgnoreCase("y") || userInput.equalsIgnoreCase("yes")){
         return  true;
     }else{
         return false;
     }
 }
+    public int getInt(int min, int max){
+        System.out.println("pick a number between" + min + "and" + max);
+        int userNum = this.scanner.nextInt();
+        if (userNum < min || userNum > max){
+            System.out.println("ops pick another number");
+            return getInt(min,max);
+        }else{
+            System.out.println(" great, you won");
+            return userNum;
+        }
+    }
 
     public static void main(String[] args) {
+
+        Input input = new Input();
+        System.out.println(input.getString());
         System.out.println("Do you like pizza?");
-        Input UserInput = new Input();
-        System.out.println(UserInput.yesNo());
+        System.out.println(input.yesNo());
+        System.out.println(input.getInt(1,10));
     }
 }
