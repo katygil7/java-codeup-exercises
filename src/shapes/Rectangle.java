167536e2f06a9620e5e64914aac0c374cb098323
katygil7 <gil.katherine22@gmail.com>
2022-08-12 15:41:33 -0500

    ..

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,10 +1,35 @@
 package shapes;
 
 public class Rectangle {
-    protected int length;
-    protected int width;
+    protected int length, width;
 
-    public Rectangle(int length) {
+
+    public Rectangle(int length, int width) {
         this.length = length;
+        this.width = width;
+    }
+    public int getArea (){
+        return length * width;
+    }
+    public int getPerimeter(){
+        return 2 * length + 2 * width;
+    }
+
+//    accessors
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
     }
 }
---
+++ src/shapes/Square.java
@@ -0,0 +1,30 @@
+package shapes;
+
+public class Square extends Rectangle{
+
+    public Square(int length, int width) {
+        super(length, width);
+    }
+    public Square(int side){
+        super(side, side);
+    }
+
+    @Override
+    public int getArea() {
+        System.out.println("square getArea");
+        return length * length;
+    }
+
+    @Override
+    public int getPerimeter() {
+        System.out.println("square getPerimeter");
+        return 4 * length;
+    }
+
+    public void setSide(int side){
+        this.length = side;
+        this.width = side;
+    }
+
+
+}
